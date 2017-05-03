8666d7adb6fad029fba8869fdd28052dbd91f561
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-02 20:16:33 -0400

    adjusting email interval and addressee

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -72,8 +72,8 @@ class Main(threading.Thread):
         while True:
             self.camera.take_capture("capture.png")
             time.sleep(5)
-            self.email.send(settings.to_field, "camera capture from %s" % (self.hostname),"test", "/home/pi/supercooler/Captures/capture.png")
-            time.sleep(3600)
+            self.email.send("ac-smart-cooler@googlegroups.com", "camera capture from %s" % (self.hostname),"test", "/home/pi/supercooler/Captures/capture.png")
+            time.sleep(7190)
 
         ###  ###
 
