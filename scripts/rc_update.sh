12a2bb513c202d034d41d08a0b927fdb4117b0fc
Dave Linnenbank <d.linnenbank@gmail.com>
2017-06-01 12:59:38 -0400

    Timed staggering now in rc.local

--- scripts/rc_update.sh
+++ scripts/rc_update.sh
@@ -2,7 +2,7 @@ echo
 echo "MOVE AND ADJUST RC.LOCAL PERMISSIONS"
 echo
 
-sudo mv /home/pi/supercooler/scripts/rc.local /etc/rc.local
+sudo cp /home/pi/supercooler/scripts/rc.local /etc/rc.local
 sudo chmod 755 /etc/rc.local
 
 exit 0
