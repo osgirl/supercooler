c10c0e81b55525821b8e5831ddb0056cb490f481
Daniel Gorelick <dqgorelick@gmail.com>
2017-06-15 15:36:34 -0400

    removes existing typo in settings

--- settings.py
+++ settings.py
@@ -1,62 +1,62 @@
-﻿ -server_hostname = "green"
- -discovery_multicastGroup = "224.3.29.71"
- -discovery_multicastPort = 10010
- -discovery_responsePort = 10011
- -pubsub_pubPort = 10012
- -pubsub_pubPort2 = 10014
- -
- -camera_unit_names = [
- -    "supercoolerA0",
- -    "supercoolerA1",
- -    "supercoolerA2",
- -    "supercoolerA3",
- -    "supercoolerA4",
- -    "supercoolerA5",
- -    "supercoolerA6",
- -    "supercoolerA7",
- -    "supercoolerA8",
- -    "supercoolerA9",
- -    "supercoolerA10",
- -    "supercoolerA11",
- -    "supercoolerB0",
- -    "supercoolerB1",
- -    "supercoolerB2",
- -    "supercoolerB3",
- -    "supercoolerB4",
- -    "supercoolerB5",
- -    "supercoolerB6",
- -    "supercoolerB7",
- -    "supercoolerB8",
- -    "supercoolerB9",
- -    "supercoolerB10",
- -    "supercoolerB11",
- -    "supercoolerC0",
- -    "supercoolerC1",
- -    "supercoolerC2",
- -    "supercoolerC3",
- -    "supercoolerC4",
- -    "supercoolerC5",
- -    "supercoolerC6",
- -    "supercoolerC7",
- -    "supercoolerC8",
- -    "supercoolerC9",
- -    "supercoolerC10",
- -    "supercoolerC11",
- -    "supercoolerD0",
- -    "supercoolerD1",
- -    "supercoolerD2",
- -    "supercoolerD3",
- -    "supercoolerD4",
- -    "supercoolerD5",
- -    "supercoolerD6",
- -    "supercoolerD7",
- -    "supercoolerD8",
- -    "supercoolerD9",
- -    "supercoolerD10",
- -    "supercoolerD11",
- -]
- -
- -conductor_names = [
- -    "supercooler-conductor",
- -    "action",
- -]
+﻿server_hostname = "green"
+discovery_multicastGroup = "224.3.29.71"
+discovery_multicastPort = 10010
+discovery_responsePort = 10011
+pubsub_pubPort = 10012
+pubsub_pubPort2 = 10014
+
+camera_unit_names = [
+    "supercoolerA0",
+    "supercoolerA1",
+    "supercoolerA2",
+    "supercoolerA3",
+    "supercoolerA4",
+    "supercoolerA5",
+    "supercoolerA6",
+    "supercoolerA7",
+    "supercoolerA8",
+    "supercoolerA9",
+    "supercoolerA10",
+    "supercoolerA11",
+    "supercoolerB0",
+    "supercoolerB1",
+    "supercoolerB2",
+    "supercoolerB3",
+    "supercoolerB4",
+    "supercoolerB5",
+    "supercoolerB6",
+    "supercoolerB7",
+    "supercoolerB8",
+    "supercoolerB9",
+    "supercoolerB10",
+    "supercoolerB11",
+    "supercoolerC0",
+    "supercoolerC1",
+    "supercoolerC2",
+    "supercoolerC3",
+    "supercoolerC4",
+    "supercoolerC5",
+    "supercoolerC6",
+    "supercoolerC7",
+    "supercoolerC8",
+    "supercoolerC9",
+    "supercoolerC10",
+    "supercoolerC11",
+    "supercoolerD0",
+    "supercoolerD1",
+    "supercoolerD2",
+    "supercoolerD3",
+    "supercoolerD4",
+    "supercoolerD5",
+    "supercoolerD6",
+    "supercoolerD7",
+    "supercoolerD8",
+    "supercoolerD9",
+    "supercoolerD10",
+    "supercoolerD11",
+]
+
+conductor_names = [
+    "supercooler-conductor",
+    "action",
+]
