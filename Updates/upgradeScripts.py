912f5e98f1aad41528786ff0ebd1bfa785e032c4
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-23 18:23:09 -0400

    preparing to update all wpa_supplicant files

--- Updates/upgradeScripts.py
+++ Updates/upgradeScripts.py
@@ -2,15 +2,6 @@ scripts = {
     "0.00":[
         "echo \"no new upgrade scripts yet\"",
     ],
-    "0.01":[
-        "cp /home/pi/supercooler/scripts/rc.local /etc/rc.local",
-    ],
-    "0.02":[
-        "cp /home/pi/supercooler/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf",
-    ],
-    "0.03":[
-    	"sudo dpkg -i /home/pi/supercooler/packages/opencv_3.2.0-1_armhf.deb",
-    ],
     "0.05":[
         "sudo cp /home/pi/supercooler/scripts/rc.local /etc/rc.local",
         "sudo chmod 755 /etc/rc.local",
