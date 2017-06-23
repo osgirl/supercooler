b4fb6b728e444cc7d2be51ac523ca6e283565dfa
Dave Linnenbank <d.linnenbank@gmail.com>
2017-06-23 17:38:58 -0400

    Wpa script revert

--- Updates/upgradeScripts.py
+++ Updates/upgradeScripts.py
@@ -14,8 +14,5 @@ scripts = {
     "0.05":[
         "sudo cp /home/pi/supercooler/scripts/rc.local /etc/rc.local",
         "sudo chmod 755 /etc/rc.local",
-    ],
-    "0.06":[
-        "sudo cp /home/pi/supercooler/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf",
     ]
 }
