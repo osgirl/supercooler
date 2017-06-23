13ed4287448872548ecb47dcff638e67e3d7b399
Dave Linnenbank <d.linnenbank@gmail.com>
2017-06-23 18:59:20 -0400

    Update script entry for new wpa_supplicant.conf

--- Updates/upgradeScripts.py
+++ Updates/upgradeScripts.py
@@ -6,7 +6,7 @@ scripts = {
         "sudo cp /home/pi/supercooler/scripts/rc.local /etc/rc.local",
         "sudo chmod 755 /etc/rc.local",
     ],
-    "0.06":[
-        "sudo cp scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf",
+    "0.07":[
+        "sudo cp /home/pi/supercooler/scripts/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf",
     ]
 }
