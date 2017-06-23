e19c798d3e86b3c25ac59c3f4fe74d9a5bce2a00
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-23 16:26:51 -0400

    getting Parse_Images working

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -196,26 +196,29 @@ class Main(threading.Thread):
     def capture_image_and_save(self, filename):
         self.camera.take_capture(filename)
 
+    def retur_env_data(self):
+        
+
     def process_images_and_report(self):
         # send images back to server
         print "process_images_and_report 1"
         filenames = [ filename for filename in os.listdir(self.capture_path) if filename.endswith(".png") ]
         print "process_images_and_report 2", filenames
-        for filename in filenames:
-            print "process_images_and_report 3", filename
-            with open("{}{}".format(self.capture_path, filename), "rb") as image_file:
-                image_data = [
-                    filename, 
-                    base64.b64encode(image_file.read())
-                ]
-                #network.send("image_capture_from_camera_unit", image_data)
+        #for filename in filenames:
+        #    print "process_images_and_report 3", filename
+        #    with open("{}{}".format(self.capture_path, filename), "rb") as image_file:
+        #        image_data = [
+        #            filename, 
+        #            base64.b64encode(image_file.read())
+        #        ]
+        #        #network.send("image_capture_from_camera_unit", image_data)
         print "process_images_and_report 4"
         # clear previous parsed capture files
         previous_parsed_capture_filenames = [ previous_parsed_capture_filename for previous_parsed_capture_filename in os.listdir(self.parsed_capture_path) if previous_parsed_capture_filename.endswith(".png") ]
         print "process_images_and_report 5", previous_parsed_capture_filenames
-        for previous_parsed_capture_filename in previous_parsed_capture_filenames:
-            print "process_images_and_report 6", previous_parsed_capture_filename
-            os.remove(previous_parsed_capture_filename)
+        #for previous_parsed_capture_filename in previous_parsed_capture_filenames:
+        #    print "process_images_and_report 6", previous_parsed_capture_filename
+        #    os.remove(previous_parsed_capture_filename)
 
         # loop through images
         for filename in filenames:
