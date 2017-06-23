bfbd83a164eb2b4da43e1d11382885dcfc115bc8
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-22 23:39:47 -0400

    a fix for cam cap

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -184,12 +184,12 @@ class Main(threading.Thread):
             topic, msg = self.queue.get(True)
             print "main.run",topic, msg
             if topic == "capture_image":
-                if msg in [0, "0"]: # on request 0, empty directory
-                    previous_filenames = [ previous_filename for previous_filename in os.listdir(self.capture_path) if previous_filename.endswith(".png") ]
-                    for previous_filename in previous_filenames:
-                        os.remove(   "{}{}".format(self.capture_path,  previous_filename) )
-                    filename = "{}_{}.png".format(self.hostname[11:], msg) 
-                    self.capture_image_and_save(filename)
+                #if msg in [0, "0"]: # on request 0, empty directory
+                #    previous_filenames = [ previous_filename for previous_filename in os.listdir(self.capture_path) if previous_filename.endswith(".png") ]
+                #    for previous_filename in previous_filenames:
+                #        os.remove(   "{}{}".format(self.capture_path,  previous_filename) )
+                filename = "{}_{}.png".format(self.hostname[11:], msg) 
+                self.capture_image_and_save(filename)
             if topic == "process_images_and_report":
                 self.process_images_and_report()
 
