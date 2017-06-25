f364171c9f4f39693302db4fc52408c551079ae3
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-25 17:06:53 -0400

    watson stub for testing with breadcrumbs

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -139,14 +139,19 @@ class Main(threading.Thread):
 
     def send_cropped_images_to_watson(self):
         visual_recognition = VisualRecognitionV3('2016-05-20', api_key='753a741d6f32d80e1935503b40a8a00f317e85c6')
+        print "send_cropped_images_to_watson 1"
         classification_data = []
         filenames = [ filename for filename in os.listdir(self.parsed_capture_path) if filename.endswith(".jpg") ]
+        print "send_cropped_images_to_watson 2", filenames
         for filename in filenames:
+            print "send_cropped_images_to_watson 3", filename
             filepath = os.path.join(self.capture_path, filename)
+            print "send_cropped_images_to_watson 4", filename
             with open( filepath, 'rb') as image_file:
                 try:
                     result = visual_recognition.classify(images_file=image_file,  classifier_ids=['beercaps_697951100'], threshold=0.99)
                     classifiers = result[u'images'][0][u'classifiers']
+                    print "send_cropped_images_to_watson 5", classifiers
                     if len(classifiers) > 0:
                         classification_data.append(
                             {
@@ -168,7 +173,7 @@ class Main(threading.Thread):
         bounds, processed_image_with_overlay, processed_image = self.parse_and_crop_images()
 
         # send images to conductor
-        send_images_to_conductor(None, processed_image, processed_image_with_overlay)
+        # send_images_to_conductor(None, processed_image, processed_image_with_overlay)
 
 
         # prepare images to send to Watson
