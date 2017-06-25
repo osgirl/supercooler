6cf110a41a8ac0045302d46481d5725353f1a5de
andycavatorta <andycavatorta@gmail.com>
2017-06-25 05:35:40 +0000

    move parsing code to parser.py

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -4,7 +4,6 @@ import commands
 import cv2
 import importlib
 import json
-import numpy as np
 import os
 import Queue
 import settings 
@@ -18,6 +17,9 @@ from thirtybirds_2_0.Network.email_simple import init as email_init
 from thirtybirds_2_0.Adaptors.Cameras.elp import init as camera_init
 from thirtybirds_2_0.Updates.manager import init as updates_init
 from thirtybirds_2_0.Network.info import init as network_info_init
+
+from parser import Image_Parser
+
 network_info = network_info_init()
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -44,112 +46,6 @@ class Thirtybirds_Client_Monitor_Client():
         pickle_version = self.get_pickle_version()
         git_timestamp = self.get_git_timestamp()
         self.network.send("client_monitor_response", (self.hostname,pickle_version, git_timestamp))
-
-class Image_Parser():
-    def __init__(self, hostname, network, min_size=65):
-        self.hostname = hostname
-        self.network = network
-        self.min_size = min_size
-        self.distortion = np.array([[-6.0e-5, 0.0, 0.0, 0.0]], np.float64)
-        self.max_confdence = 1.0
-    
-    def equalize_histogram(self, img):
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        equalized = clahe.apply(img)
-        _, thresh = cv2.threshold(equalized, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)
-        return thresh
-
-    def process_split(self, img):
-        def process_channel(img):
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-            equalized = clahe.apply(img)
-            thresh = cv2.adaptiveThreshold(equalized, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 65, 17)
-            opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, None)
-            return opening
-
-        b, g, r = map(process_channel, cv2.split(img))
-        cv2.bitwise_and(b, g, b)
-        processed = cv2.bitwise_and(b, r)
-        processed = cv2.bilateralFilter(processed, 9, 100, 100)
-        return processed
-
-    def mask_blobs(self, gray):
-        mask = np.zeros(gray.shape[:2], dtype='uint8')
-        # detect blobs
-        #mser = cv2.MSER_create(_delta=4, _min_area=65, _max_area=14400, _max_variation=1.0)
-        #blobs, _ = mser.detectRegions(gray)
-        mser = cv2.MSER_create(_delta=4, _min_area=65, _max_area=14400, _max_variation=1.0)
-        blobs, _ = mser.detectRegions(gray)
-        # find circular blobs
-        for blob in blobs:
-            hull = cv2.convexHull(blob.reshape(-1, 1, 2))
-            epsilon = 0.01*cv2.arcLength(hull, True)
-            poly = cv2.approxPolyDP(hull, epsilon, True)
-            # select polygons with more than 9 vertices
-            if len(poly) > 9: 
-                cv2.polylines(mask, [blob], 1, 255, 1)
-        return mask
-
-    def calc_camera_matrix(self, (height,width)):
-        cam = np.eye(3, dtype=np.float32) # assume unit matrix for camera
-        cam[0, 2] = width  / 2.0  # center x
-        cam[1, 2] = height / 2.0  # center y
-        cam[0, 0] = 10.0          # focal length x
-        cam[1, 1] = 10.0          # focal length y
-        return cam
-
-    def mask_beers(self, img, camera_matrix):
-        # create masks to accumulate blobs detected by each pass
-        mask_distorted = np.zeros(img.shape[:2], dtype = 'uint8')
-        mask = np.zeros(img.shape[:2], dtype = 'uint8')
-        # distorted:
-        # CLAHE and Otsu threshold
-        equalized = self.equalize_histogram(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-        mask_distorted += self.mask_blobs(equalized)
-        # adaptive threshold
-        processed = self.process_split(img)
-        mask_distorted += self.mask_blobs(processed)
-        # undistorted:
-        img_out = cv2.undistort(img, camera_matrix, self.distortion )
-        # CLAHE and Otsu threshold
-        equalized = self.equalize_histogram(cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY))
-        mask += self.mask_blobs(equalized)
-        # adaptive threshold
-        processed = self.process_split(img_out)
-        mask += self.mask_blobs  (processed)
-        # undistort results from distorted image; sum with undistorted results
-        mask += cv2.undistort(mask_distorted, camera_matrix, self.distortion )
-        return mask
-
-    def find_beers(self, mask, vis):
-        _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        result = []
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            if max(w, h) < self.min_size: continue
-            (cx,cy), radius = cv2.minEnclosingCircle(contour)
-            center = (int(cx),int(cy))
-            radius = int(radius)
-            circlePoints = cv2.ellipse2Poly(center, (radius,radius), 0, 0, 360, 1)
-            confidence = cv2.matchShapes(contour, circlePoints, 1, 0.0)
-            if confidence > self.max_confdence: continue
-            result.append((x, y, w, h))
-
-            cv2.polylines(vis, [contour], 0, (0,0,255), 1)
-            cv2.rectangle(vis, (x,y), (x+w,y+h), (0,255,0), 2)
-            cv2.circle(vis, center, radius, (0,255,0), 2)                      
-            cv2.putText(vis, '%.3f' % confidence, center, cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,255), 2)
-        #if self.interactive: plt.imshow(vis), plt.show()
-        return (result, vis)
-
-    def parse(self, filename, camera_matrix):
-        img = cv2.imread(filename)
-        img_weighted = img.copy()
-        camera_matrix = self.calc_camera_matrix(img.shape[:2])
-        mask_final = self.mask_beers(img, camera_matrix)
-        img_out = cv2.undistort(img, camera_matrix, self.distortion )
-        beer_bounds, vis = self.find_beers(mask_final, img_out.copy())
-        return beer_bounds, vis, img_out
 
 class Main(threading.Thread):
     def __init__(self, hostname, network):
