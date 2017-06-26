49448b50f2756b38cbb063c305697ca098357416
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-25 20:12:28 -0400

    updating upgrade scripts to omit commands that were just applied manually

--- Updates/upgradeScripts.py
+++ Updates/upgradeScripts.py
@@ -5,12 +5,5 @@ scripts = {
     "0.05":[
         "sudo cp /home/pi/supercooler/scripts/rc.local /etc/rc.local",
         "sudo chmod 755 /etc/rc.local",
-    ],
-    "0.095":[
-        "sudo apt-get update",
-        "sudo apt-get -f install",
-        "sudo apt-get install  -y --force-yes build-essential libssl-dev libffi-dev",
-        "sudo pip install --upgrade watson-developer-cloud",
-        "sudo apt-get install -y zip"
     ]
 }
