48f2075ea4fcce2ee56054b3dea12e133d3db1b2
Dave Linnenbank <d.linnenbank@gmail.com>
2017-06-30 12:39:49 -0400

    Removing auth file line from gdrive install script

--- Updates/upgradeScripts.py
+++ Updates/upgradeScripts.py
@@ -11,6 +11,5 @@ scripts = {
 		"sudo chmod +x /home/pi/gdrive",
 		"sudo install /home/pi/gdrive /usr/local/bin/gdrive",
 		"mkdir /home/pi/.gdrive",
-		"wget -P /home/pi/.gdrive/ http://theproblemislastyear.com/u23mkhJsVUPNJHnOYQJnM7arOAcEjkC2qdngPOOqnAafc2rqOSwPtFNf3FS2j4gh/token_v2.json",
     ]
 }
