6a63e87ab6c4dd99b7c81013268b9a9c0b2863c9
Andy Cavatorta <andycavatorta@gmail.com>
2017-07-01 19:01:59 -0400

    fix syntax error

--- Roles/camera_unit/main.py
+++ Roles/camera_unit/main.py
@@ -273,7 +273,7 @@ class Main(threading.Thread):
             topic, msg = self.queue.get(True)
             # print "Main.run 2"
             if topic == "capture_image":
-                print ">>>>>>>>>>>>>>" repr(msg)
+                print ">>>>>>>>>>>>>>", repr(msg)
                 light_level, timestamp = msg[0],msg[1]
                 if light_level in [0, "0"]: # on request 0, empty directory
                     previous_filenames = [ previous_filename for previous_filename in os.listdir(self.capture_path) if previous_filename.endswith(".png") ]
